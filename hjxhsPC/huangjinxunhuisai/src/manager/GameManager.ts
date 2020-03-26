b2093ae1d79c1fec3fb77513689dc49ecf490a59
wq <442816542@qq.com>
2020-03-26 18:17:05 +0800

    2020年3月26日 18:16:30

--- hjxhsPC/huangjinxunhuisai/src/manager/GameManager.ts
+++ hjxhsPC/huangjinxunhuisai/src/manager/GameManager.ts
@@ -141,7 +141,7 @@ class GameManager extends egret.EventDispatcher {
 	 * 游戏正式结束通知
 	 */
 	public onLogicGameOver(): void { 
-		console.log('游戏结束');
+		console.log('游戏结束'); 
 
 		this.gameState = GameType.GameState.STOP;
 		this.isStart = true;
